b0be671106a803da891e98817217c5c54e28706e
PITBULL <rosolovskiy2005@gmail.com>
2022-03-27 17:09:41 +0300

    version 1.0

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { Component } from 'react/cjs/react.development';
+import React, { Component } from 'react/cjs/react.development';
 import MarvelService from './../../servises/MarvelService';
 import ErrorMessage from '../errorMessage/errorMesssage';
 import Spinner from './../Spinner/Spinner';
@@ -20,29 +20,36 @@ class CharList extends Component {
 	componentDidMount() {
 		this.onRequest();
 	}
+
 	onRequest = offset => {
+		this.onCharListLoading();
 		this.marvelService
 			.getAllCharacters(offset)
 			.then(this.onCharListLoaded)
 			.catch(this.onError);
 	};
+
 	onCharListLoading = () => {
 		this.setState({
 			newItemLoading: true,
 		});
 	};
+
 	onCharListLoaded = newCharList => {
 		let ended = false;
 		if (newCharList.length < 9) {
 			ended = true;
 		}
+
 		this.setState(({ offset, charList }) => ({
 			charList: [...charList, ...newCharList],
 			loading: false,
 			newItemLoading: false,
-			offset: (offset += 9),
+			offset: offset + 9,
+			charEnded: ended,
 		}));
 	};
+
 	onError = () => {
 		this.setState({
 			error: true,
@@ -50,10 +57,31 @@ class CharList extends Component {
 		});
 	};
 
+	itemRefs = [];
+
+	setRef = ref => {
+		this.itemRefs.push(ref);
+	};
+
+	focusOnItem = id => {
+		// Я реализовал вариант чуть сложнее, и с классом и с фокусом
+		// Но в теории можно оставить только фокус, и его в стилях использовать вместо класса
+		// На самом деле, решение с css-классом можно сделать, вынеся персонажа
+		// в отдельный компонент. Но кода будет больше, появится новое состояние
+		// и не факт, что мы выиграем по оптимизации за счет бОльшего кол-ва элементов
+
+		// По возможности, не злоупотребляйте рефами, только в крайних случаях
+		this.itemRefs.forEach(item =>
+			item.classList.remove('char__item_selected')
+		);
+		this.itemRefs[id].classList.add('char__item_selected');
+		this.itemRefs[id].focus();
+	};
+
 	// Этот метод создан для оптимизации,
 	// чтобы не помещать такую конструкцию в метод render
 	renderItems(arr) {
-		const items = arr.map(item => {
+		const items = arr.map((item, i) => {
 			let imgStyle = { objectFit: 'cover' };
 			if (
 				item.thumbnail ===
@@ -65,8 +93,19 @@ class CharList extends Component {
 			return (
 				<li
 					className='char__item'
+					tabIndex={0}
+					ref={this.setRef}
 					key={item.id}
-					onClick={() => this.props.onCharSelected(item.id)}
+					onClick={() => {
+						this.props.onCharSelected(item.id);
+						this.focusOnItem(i);
+					}}
+					onKeyPress={e => {
+						if (e.key === ' ' || e.key === 'Enter') {
+							this.props.onCharSelected(item.id);
+							this.focusOnItem(i);
+						}
+					}}
 				>
 					<img
 						src={item.thumbnail}
@@ -82,7 +121,7 @@ class CharList extends Component {
 	}
 
 	render() {
-		const { charList, loading, error, newItemLoading, offset, charEnded } =
+		const { charList, loading, error, offset, newItemLoading, charEnded } =
 			this.state;
 
 		const items = this.renderItems(charList);
@@ -99,8 +138,8 @@ class CharList extends Component {
 				<button
 					className='button button__main button__long'
 					disabled={newItemLoading}
-					onClick={() => this.onRequest(offset)}
 					style={{ display: charEnded ? 'none' : 'block' }}
+					onClick={() => this.onRequest(offset)}
 				>
 					<div className='inner'>load more</div>
 				</button>
